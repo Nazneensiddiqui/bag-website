0143ae977c95b82b860f68ca93624f755828b4db
naneen <nazneenbanosiddiqui@gmail.com>
2024-08-04 11:29:13 +0530

    javascript commit

---
+++ imgjava.js
@@ -0,0 +1,22 @@
+function fun(){
+    let img = document.getElementById("chang_image")
+    img.src="g1.webp"
+}
+function fun1(){
+    let img = document.getElementById("chang_image")
+    img.src="g2.webp"
+}
+function fun2(){
+    let img = document.getElementById("chang_image")
+    img.src="g3.webp"
+}
+function fun3(){
+    let img = document.getElementById("chang_image")
+    img.src="g4.webp"
+}
+function fun4(){
+    let img = document.getElementById("chang_image")
+    img.src="g5.webp"
+}
+
+
